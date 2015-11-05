e7e838277c924974796c4f157f79f9da2d1a0752
Joel Dueck <joel@jdueck.net>
2015-11-05 17:15:39 -0600

    formally implement base64 encoding

--- set1-basics.rkt
+++ set1-basics.rkt
@@ -17,7 +17,7 @@
 (define (base64char n)
   (string-ref "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/" n))
 
-; helper function to split a flat list into a list of lists where each
+; Generic helper: split a flat list into a list of lists where each
 ; contained list has the specified number (chunk-size) of elements.
 (define (split-into chunk-size arg-list)
   (cond
@@ -27,16 +27,35 @@
      (let-values ([(first-part second-part) (split-at arg-list chunk-size)])
        (cons first-part (split-into chunk-size second-part)))]))
 
-(define (encode-base64-packet byte-list)
-  (let ([byte-str (list->bytes byte-list)])
-    (+ (arithmetic-shift (bytes-ref byte-str 0) 16)
-       (arithmetic-shift (bytes-ref byte-str 1) 8)
-       (bytes-ref byte-str 2))))
+; Generic helper: pad a list with zeros so that the element count is divisible by three
+(define (make-triplet ls)
+  (cond
+    [(equal? 3 (length ls)) ls]
+    [else
+     (flatten (list ls
+                    (build-list (- 3 (modulo (length ls) 3))
+                                (lambda(x) (values 0)))))]))
 
-(define (my-base64-result num)
-  (list->string (list (base64char (bitwise-and (arithmetic-shift num -18) 63))
-                      (base64char (bitwise-and (arithmetic-shift num -12) 63))
-                      (base64char (bitwise-and (arithmetic-shift num -6) 63))
-                      (base64char (bitwise-and num 63)))))
+(define (hex-string->base64-string src-str)
 
-; [NOT YET COMPLETE]
+  ; task-specific helper: converts a single 24-bit number into a 4-char base64 string
+  (define (b64-result num)
+    (list->string (list (base64char (bitwise-and (arithmetic-shift num -18) 63))
+                        (base64char (bitwise-and (arithmetic-shift num -12) 63))
+                        (base64char (bitwise-and (arithmetic-shift num -6) 63))
+                        (base64char (bitwise-and num 63)))))  
+
+  ; task-specific helper: encodes a set of 3 or fewer bytes into a padded base64 string
+  (define (encode-base64-packet byte-list)
+    (let ([byte-str (list->bytes (make-triplet byte-list))])
+      (b64-result (+ (arithmetic-shift (bytes-ref byte-str 0) 16)
+                     (arithmetic-shift (bytes-ref byte-str 1) 8)
+                     (bytes-ref byte-str 2)))))
+  
+  ; glue it all together:
+  (string-join (map encode-base64-packet
+                    (split-into 3 (bytes->list (hex-string->bytes src-str))))
+               ""))
+
+; NOT YET COMPLETE
+;  Functional but does not yet handle padding when strlen % 3 <> 0
